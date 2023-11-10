ee56e6de71fdf98bdeacaac3069fb2df1066c738
Kota1021 <ko_matsumoto@yumemi.co.jp>
2023-11-10 15:59:43 +0900

    fixed Tests to work properly

--- Example/ExampleTests/WeatherViewControllerTests.swift
+++ Example/ExampleTests/WeatherViewControllerTests.swift
@@ -14,11 +14,14 @@ class WeatherViewControllerTests: XCTestCase {
 
     var weahterViewController: WeatherViewController!
     var weahterModel: WeatherModelMock!
+    var disasterModel: DisasterModelMock!
     
     override func setUpWithError() throws {
         weahterModel = WeatherModelMock()
+        disasterModel = DisasterModelMock()
         weahterViewController = R.storyboard.weather.instantiateInitialViewController()!
         weahterViewController.weatherModel = weahterModel
+        weahterViewController.disasterModel = disasterModel
         _ = weahterViewController.view
     }
 
@@ -27,51 +30,87 @@ class WeatherViewControllerTests: XCTestCase {
     }
 
     func test_天気予報がsunnyだったらImageViewのImageにsunnyが設定されること_TintColorがredに設定されること() throws {
-        weahterModel.fetchWeatherImpl = { _ in
-            Response(weather: .sunny, maxTemp: 0, minTemp: 0, date: Date())
-        }
+        weahterModel.fetchedWeatherStub = {
+            let response = Response(weather: .sunny, maxTemp: 0, minTemp: 0, date: Date())
+            return Result<Response, WeatherError>.success(response)
+        }()
         
-        weahterViewController.loadWeather()
-        XCTAssertEqual(weahterViewController.weatherImageView.tintColor, R.color.red())
-        XCTAssertEqual(weahterViewController.weatherImageView.image, R.image.sunny())
+        weahterViewController.loadWeather(nil)
+        
+        //XCTest内での非同期処理の呼び出し方の参考: https://qiita.com/chocoyama/items/620c7b0b6ec9e235e9ea
+        let expectation = self.expectation(description: "wait for async task")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            XCTAssertEqual(self.weahterViewController.weatherImageView.tintColor, R.color.red())
+            XCTAssertEqual(self.weahterViewController.weatherImageView.image, R.image.sunny())
+            expectation.fulfill()
+        }
+        // fullfillメソッドが呼び出されるまでテストを終了させずに最大0.2秒間待機する
+        self.wait(for: [expectation], timeout: 0.2)
     }
     
     func test_天気予報がcloudyだったらImageViewのImageにcloudyが設定されること_TintColorがgrayに設定されること() throws {
-        weahterModel.fetchWeatherImpl = { _ in
-            Response(weather: .cloudy, maxTemp: 0, minTemp: 0, date: Date())
-        }
+        weahterModel.fetchedWeatherStub = {
+            let response = Response(weather: .cloudy, maxTemp: 0, minTemp: 0, date: Date())
+            return Result<Response, WeatherError>.success(response)
+        }()
         
-        weahterViewController.loadWeather()
-        XCTAssertEqual(weahterViewController.weatherImageView.tintColor, R.color.gray())
-        XCTAssertEqual(weahterViewController.weatherImageView.image, R.image.cloudy())
+        weahterViewController.loadWeather(nil)
+        //XCTest内での非同期処理の呼び出し方の参考: https://qiita.com/chocoyama/items/620c7b0b6ec9e235e9ea
+        let expectation = self.expectation(description: "wait for async task")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            XCTAssertEqual(self.weahterViewController.weatherImageView.tintColor, R.color.gray())
+            XCTAssertEqual(self.weahterViewController.weatherImageView.image, R.image.cloudy())
+            expectation.fulfill()
+        }
+        // fullfillメソッドが呼び出されるまでテストを終了させずに最大0.2秒間待機する
+        self.wait(for: [expectation], timeout: 0.2)
     }
     
     func test_天気予報がrainyだったらImageViewのImageにrainyが設定されること_TintColorがblueに設定されること() throws {
-        weahterModel.fetchWeatherImpl = { _ in
-            Response(weather: .rainy, maxTemp: 0, minTemp: 0, date: Date())
-        }
+        weahterModel.fetchedWeatherStub = {
+            let response = Response(weather: .rainy, maxTemp: 0, minTemp: 0, date: Date())
+            return Result<Response, WeatherError>.success(response)
+        }()
         
-        weahterViewController.loadWeather()
-        XCTAssertEqual(weahterViewController.weatherImageView.tintColor, R.color.blue())
-        XCTAssertEqual(weahterViewController.weatherImageView.image, R.image.rainy())
+        weahterViewController.loadWeather(nil)
+        //XCTest内での非同期処理の呼び出し方の参考: https://qiita.com/chocoyama/items/620c7b0b6ec9e235e9ea
+        let expectation = self.expectation(description: "wait for async task")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            XCTAssertEqual(self.weahterViewController.weatherImageView.tintColor, R.color.blue())
+            XCTAssertEqual(self.weahterViewController.weatherImageView.image, R.image.rainy())
+            expectation.fulfill()
+        }
+        // fullfillメソッドが呼び出されるまでテストを終了させずに最大0.2秒間待機する
+        self.wait(for: [expectation], timeout: 0.2)
     }
     
     func test_最高気温_最低気温がUILabelに設定されること() throws {
-        weahterModel.fetchWeatherImpl = { _ in
-            Response(weather: .rainy, maxTemp: 100, minTemp: -100, date: Date())
-        }
+        weahterModel.fetchedWeatherStub = {
+            let response = Response(weather: .sunny, maxTemp: 100, minTemp: -100, date: Date())
+            return Result<Response, WeatherError>.success(response)
+        }()
         
-        weahterViewController.loadWeather()
-        XCTAssertEqual(weahterViewController.minTempLabel.text, "-100")
-        XCTAssertEqual(weahterViewController.maxTempLabel.text, "100")
+        weahterViewController.loadWeather(nil)
+        //XCTest内での非同期処理の呼び出し方の参考: https://qiita.com/chocoyama/items/620c7b0b6ec9e235e9ea
+        let expectation = self.expectation(description: "wait for async task")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            XCTAssertEqual(self.weahterViewController.minTempLabel.text, "-100")
+            XCTAssertEqual(self.weahterViewController.maxTempLabel.text, "100")
+            expectation.fulfill()
+        }
+        // fullfillメソッドが呼び出されるまでテストを終了させずに最大0.2秒間待機する
+        self.wait(for: [expectation], timeout: 0.2)
     }
 }
 
 class WeatherModelMock: WeatherModel {
-    
-    var fetchWeatherImpl: ((Request) throws -> Response)!
-    
-    func fetchWeather(_ request: Request) throws -> Response {
-        return try fetchWeatherImpl(request)
+    func fetchWeather(at area: String, date: Date, completion: @escaping (Result<Example.Response, Example.WeatherError>) -> Void) {
+        completion( fetchedWeatherStub )
     }
+    
+    var fetchedWeatherStub: Result<Response, WeatherError>!
+}
+
+class DisasterModelMock: DisasterModel {
+    func fetchDisaster(completion: ((String) -> Void)?) {}
 }
